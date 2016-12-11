22ccfd4cccff903f7981c522f9f9f7abbe1cbe04
Marius Landwehr <marius.landwehr@gmail.com>
2016-12-11 13:40:04 +0100

    Added better Access Control

--- TinyConsole/TinyConsoleController.swift
+++ TinyConsole/TinyConsoleController.swift
@@ -8,34 +8,25 @@
 
 import UIKit
 
-
-/// the kind of window modes that are supported by TinyConsole
-///
-/// - collapsed: the console is hidden
-/// - expanded: the console is shown
-enum TinyConsoleWindowMode {
-    case collapsed
-    case expanded
-}
-
 open class TinyConsoleController: UIViewController {
     
-    // MARK: - Public Properties -
-    var rootViewController: UIViewController?
+    /// the kind of window modes that are supported by TinyConsole
+    ///
+    /// - collapsed: the console is hidden
+    /// - expanded: the console is shown
+    enum WindowMode {
+        case collapsed
+        case expanded
+    }
     
-    var consoleViewController: TinyConsoleViewController = {
+    // MARK: - Private Properties -
+    private var rootViewController: UIViewController
+    
+    private var consoleViewController: TinyConsoleViewController = {
         return TinyConsoleViewController()
     }()
     
-    var consoleWindowMode: TinyConsoleWindowMode = .collapsed {
-        didSet {
-            consoleViewHeightConstraint?.isActive = false
-            consoleViewHeightConstraint?.constant = consoleWindowMode == .collapsed ? 0 : self.expandedHeight
-            consoleViewHeightConstraint?.isActive = true
-        }
-    }
-    
-    lazy var consoleViewHeightConstraint: NSLayoutConstraint? = {
+    private lazy var consoleViewHeightConstraint: NSLayoutConstraint? = {
         if #available(iOS 9, *) {
             return self.consoleViewController.view.heightAnchor.constraint(equalToConstant: 0)
         } else {
@@ -43,7 +34,6 @@ open class TinyConsoleController: UIViewController {
         }
     }()
     
-    // MARK: - Private Properties -
     private let consoleFrameHeight: CGFloat = 120
     private let expandedHeight: CGFloat = 140
     
@@ -55,6 +45,14 @@ open class TinyConsoleController: UIViewController {
         return consoleFrame
     }()
     
+    private var consoleWindowMode: WindowMode = .collapsed {
+        didSet {
+            consoleViewHeightConstraint?.isActive = false
+            consoleViewHeightConstraint?.constant = consoleWindowMode == .collapsed ? 0 : self.expandedHeight
+            consoleViewHeightConstraint?.isActive = true
+        }
+    }
+    
     // MARK: - Initializer -
     public init(rootViewController: UIViewController) {
         self.rootViewController = rootViewController
@@ -62,6 +60,8 @@ open class TinyConsoleController: UIViewController {
     }
     
     required public init?(coder aDecoder: NSCoder) {
+        assertionFailure("Interface Builder is not supported")
+        self.rootViewController = UIViewController()
         super.init(coder: aDecoder)
     }
     
@@ -74,35 +74,12 @@ open class TinyConsoleController: UIViewController {
         view.addSubview(consoleViewController.view)
         consoleViewController.didMove(toParentViewController: self)
         
-        if let content = rootViewController {
-            addChildViewController(content)
-            content.view.frame = CGRect(x: consoleFrame.minX, y: consoleFrame.maxY, width: view.bounds.width, height: 120)
-            view.addSubview(content.view)
-            content.didMove(toParentViewController: self)
-        }
+        addChildViewController(rootViewController)
+        rootViewController.view.frame = CGRect(x: consoleFrame.minX, y: consoleFrame.maxY, width: view.bounds.width, height: 120)
+        view.addSubview(rootViewController.view)
+        rootViewController.didMove(toParentViewController: self)
         
         setupConstraints()
-    }
-    
-    func setupConstraints() {
-        
-        guard let rootViewControllerView = rootViewController?.view else {
-            print("You forgot to setup the rootViewController for TinyConsole")
-            return
-        }
-        
-        rootViewControllerView.attach(anchor: .top, to: view)
-        
-        consoleViewController.view.attach(anchor: .bottom, to: view)
-        consoleViewHeightConstraint?.isActive = true
-        
-        if #available(iOS 9, *) {
-            
-            rootViewController?.view.bottomAnchor.constraint(equalTo: consoleViewController.view.topAnchor).isActive = true
-        } else {
-            
-            NSLayoutConstraint(item: (rootViewController?.view)!, attribute: .bottom, relatedBy: .equal, toItem: consoleViewController.view, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        }
     }
     
     open override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
@@ -111,6 +88,30 @@ open class TinyConsoleController: UIViewController {
             UIView.animate(withDuration: 0.25) {
                 self.view.layoutIfNeeded()
             }
+        }
+    }
+    
+    // MARK: - Private Methods -
+    private func setupConstraints() {
+        
+        rootViewController.view.attach(anchor: .top, to: view)
+        
+        consoleViewController.view.attach(anchor: .bottom, to: view)
+        consoleViewHeightConstraint?.isActive = true
+        
+        if #available(iOS 9, *) {
+            
+            rootViewController.view.bottomAnchor.constraint(equalTo: consoleViewController.view.topAnchor).isActive = true
+        } else {
+            
+            NSLayoutConstraint(item: (rootViewController.view)!,
+                               attribute: .bottom,
+                               relatedBy: .equal,
+                               toItem: consoleViewController.view,
+                               attribute: .top,
+                               multiplier: 1.0,
+                               constant: 0)
+                .isActive = true
         }
     }
 }
@@ -142,15 +143,43 @@ fileprivate extension UIView {
             
             switch anchor {
             case .top:
-                NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+                NSLayoutConstraint(item: self,
+                                   attribute: .top,
+                                   relatedBy: .equal,
+                                   toItem: view,
+                                   attribute: .top,
+                                   multiplier: 1.0,
+                                   constant: 0)
+                    .isActive = true
             case .bottom:
-                NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+                NSLayoutConstraint(item: self,
+                                   attribute: .bottom,
+                                   relatedBy: .equal,
+                                   toItem: view,
+                                   attribute: .bottom,
+                                   multiplier: 1.0,
+                                   constant: 0)
+                    .isActive = true
             }
             
             // left anchor
-            NSLayoutConstraint(item: self, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: self,
+                               attribute: .left,
+                               relatedBy: .equal,
+                               toItem: view,
+                               attribute: .left,
+                               multiplier: 1.0,
+                               constant: 0)
+                .isActive = true
             // right anchor
-            NSLayoutConstraint(item: self, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 0).isActive = true
+            NSLayoutConstraint(item: self,
+                               attribute: .right,
+                               relatedBy: .equal,
+                               toItem: view,
+                               attribute: .right,
+                               multiplier: 1.0,
+                               constant: 0)
+                .isActive = true
         }
     }
 }
